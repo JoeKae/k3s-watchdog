227092257a04184b39f77f2ab37d531730d0bf15
Joe <52297861+JoeKae@users.noreply.github.com>
2022-07-11 16:19:00 +0200

    dev

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ cmdpipe = subprocess.Popen(
     shell=True)
 result = {}
 for row in cmdpipe.stdout.readline():
-    if 'CrashLoopBackOff' in row:
-        print(row)
-        print("restart")
-
+    # if 'CrashLoopBackOff' in row:
+    #     print(row)
+    #     print("restart")
+    print(row)
 # You need to close the file handles, as they will stay open "forever" otherwise.
 cmdpipe.stdout.close()
 cmdpipe.stderr.close()
