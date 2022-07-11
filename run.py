ba24ec70f1f91657168a9512e72ad886bd1fb8fe
Joe <52297861+JoeKae@users.noreply.github.com>
2022-07-11 16:37:13 +0200

    dev

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
+"""
+Checks for CrashLoopBackOff status in podstatus of kubernetes namespace kube-system
+Restarts the service if found
+"""
 import subprocess
+import sys
+
 from loguru import logger
 
-output = subprocess.check_output(
-    "kubectl get pods -n kube-system | grep CrashLoopBackOff",
-    shell=True
-).decode()
-result = {}
-for row in output.splitlines():
+try:
+    OUTPUT = subprocess.check_output(
+        "kubectl get pods -n kube-system | grep CrashLoopBackOff",
+        shell=True
+    ).decode()
+except subprocess.CalledProcessError:
+    sys.exit(0)
+
+for row in OUTPUT.splitlines():
     if 'CrashLoopBackOff' in row:
         logger.info('restarting k3s service')
         subprocess.call(['sudo', 'systemctl', 'restart',  'k3s'])
-        exit(0)
-
+        sys.exit(0)
